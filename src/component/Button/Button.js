db210436e88d2ff9576c55e972e0132c79551d91
Le Thang <lethang@Les-iMac.local>
2021-01-05 10:06:18 +0700

    fix 2

--- src/component/Button/Button.js
+++ src/component/Button/Button.js
@@ -3,7 +3,7 @@ import "./Button.scss";
 const Button = () => {
     return (
         <div className="button" >
-            <a href="https://drive.google.com/file/d/1Qf6nAITc8eSVfrFYzVmxfX4SHRYVoGKR/view?usp=sharing">DOWNLOAD MY CV!</a>
+            <a href="https://drive.google.com/file/d/10fSoJnOPy2JnfwrFXob6o7IEe7I7QS0n/view?usp=sharing">DOWNLOAD MY CV!</a>
         </div>
     )
 }
